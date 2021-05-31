6b6b71045ae89171912385c7b04e21c1645d0241
18harshgandhi@gmail.com <18harshgandhi@gmail.com>
2021-05-31 10:27:59 +0530

    Update Header.js

--- google-clone-app/components/Header.js
+++ google-clone-app/components/Header.js
@@ -32,7 +32,7 @@ function Header() {
             />
             <form className="flex px-6 py-3 ml-10 mr-5 border-gray-200 rounded-full shadow-lg max-w-3xl items-center flex-grow">
                 
-                <input type="text" ref={searchInputRef} className="flex-grow w-full focus:outline-none" />
+                <input type="text" ref={searchInputRef} className="flex-grow w-full focus:outline-none" defaultValue={router.query.term} />
 
                 <XIcon className="h-7 sm:mr-3 text-gray-500 cursor-pointer transition duration-100 transform hover:scale-125"
                     onClick={() => { searchInputRef.current.value = "" }} />
